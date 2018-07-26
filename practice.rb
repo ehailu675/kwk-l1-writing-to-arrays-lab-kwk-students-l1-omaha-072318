8f081c8d100df03d83187b1aacf292cee1295cdc
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 15:28:24 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -18,3 +18,14 @@ cart_item_prices.each do |price|
   tax_included << price_with_tax
 end
 puts tax_included
+
+
+big_ticket_prices[]
+
+cart_item_prices = [ 12.50, 19.99, 3.49, 7.99]
+cart_item_prices.each do |price|
+  if price >= 15
+    big_ticket_prices << price
+  end
+end
+puts big_ticket_prices
