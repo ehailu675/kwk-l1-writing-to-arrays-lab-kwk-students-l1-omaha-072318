302119300c87c9e2b516b4450fddd4ec16d675a7
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 16:10:23 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,14 +1,13 @@
 rainbow_colors = ["yellow", "default", "light_cyan"]
 
 def change_rainbow_colors
- do  rainbow_colors.delete("yellow")
+  rainbow_colors.delete("yellow")
   rainbow_colors.delete("default")
   rainbow_colors.delete("light_cyan")
   rainbow_colors[0] = "red"
   rainbow_colors[1] = "light_red"
   rainbow_colors[2] = "light_yellow"
 end 
- end 
  puts rainbow_colors
 
 def add_colors
