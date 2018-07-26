dd421ff45eaaf88299b402dec761ddabd664b901
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 15:14:45 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -11,7 +11,7 @@
 # 
 
 # 
-cart_item_prices = [12.43, 19.99, 3.49, 75.00]
+cart_item_prices = [12.43, 19.99, 3.49, 75.00, 84.00, 20.00]
 tax_included = [ ]
 cart_item_prices.each do |price|
   price_with_tax = price * 1.17
