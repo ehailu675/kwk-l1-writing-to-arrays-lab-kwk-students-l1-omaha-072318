5978bad82b44a86b487f320171df913be0e31263
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 15:31:53 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -21,7 +21,7 @@
 
 
 big_ticket_prices = []
-cart_item_prices = [12.50, 19.99, 3.49, 7.99]
+cart_item_prices = [15.50, 19.99, 3.49, 7.99]
 cart_item_prices.each do |price|
   if price >=15
   big_ticket_prices << price
