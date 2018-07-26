fadedea9a0df75c5678679e3e75f30a1f1a82d52
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 15:05:26 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -3,12 +3,16 @@
 #   puts shark
 # end
 
-# arksh
-# endsharks = ["Hammerhead", "Great White", "Tiger", "Whale"]
+# sharks = ["Hammerhead", "Great White", "Tiger", "Whale"]
 # sharks.each do |shark|
-#   puts 
+#   puts sharks
+# end 
 
-siblings = ["ruth", "hannah"]
-siblings.each do |x|
-  puts "hey x"
+# 
+
+count = 1
+cart_item_prices = [12.43, 19.99, 3.49, 75.00]
+cart_item_prices.each do |price|
+  puts "Item #{count}: #{price}"
+  count += 1
 end
