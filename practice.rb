e342342d31677add786d4d5ec67b8065dd1bd8d3
ehailu675 <elisabethahailu@gmail.com>
2018-07-26 14:53:14 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -1,9 +1,9 @@
-sharks = ["Hammerhead", "Great White", "Tiger", "Whale"]
-for shark in sharks do
-  puts shark
-end
-
 # sharks = ["Hammerhead", "Great White", "Tiger", "Whale"]
-# sharks.each do |shark|
+# for shark in sharks do
 #   puts shark
 # end
+
+sharks = ["Hammerhead", "Great White", "Tiger", "Whale"]
+sharks.each do |shark|
+  puts shark
+end
